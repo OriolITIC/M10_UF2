76e0dc5b764372ee884046460407db7db9fbacb5
oriol <2023_oriol.martinez@iticbcn.cat>
2024-01-18 16:44:52 +0100

    .

--- REPAS/exercici14.py
+++
@@ -1,9 +0,0 @@
-numeros = input("Introdueix deu numeros separats per espai: ")
-numeros_llista = numeros.split()
-tupla_numeros = tuple()
-
-for numero in numeros_llista:
-    tupla_numeros += (int(numero),)  
-
-print("La tupla de números és:", tupla_numeros)
-print(type(tupla_numeros))
