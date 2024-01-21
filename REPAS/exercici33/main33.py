a23082540a6e4b53aabab41366912fd837097337
oriol <2023_oriol.martinez@iticbcn.cat>
2024-01-21 21:05:29 +0100

    36-39

--- REPAS/exercici33/main33.py
+++
@@ -1,7 +0,0 @@
-import exercici33
-
-iva = float(input("Introdueix l'IVA a aplicar: "))
-
-llista_compra = {100: 10, 300: 5, 1000: 30}
-
-exercici33.calcular_total_amb_descompte_i_iva(llista_compra,iva)
--- REPAS/exercici34/exercici34.py
+++
@@ -1,9 +0,0 @@
-def calcular_quadrat(numero):
-    return numero ** 2
-
-def calcular_quadrats(llista_numeros):
-    llista_quadrats = []
-
-    for numero in llista_numeros:
-        llista_quadrats.append(calcular_quadrat(numero))
-    return llista_quadrats
