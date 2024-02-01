d88cbb588a63c3ad7ba302e8d95625e042448c90
Oriol Martinez <2023_oriol.martinez@iticbcn.cat>
2024-02-01 16:43:18 +0100

    Pujant classe main buida i més configuració de connexió

--- PRACTICA2/docker/connection.py
+++ PRACTICA2/docker/connection.py
@@ -13,4 +13,15 @@ conn = psycopg2.connect(
 # Per fer la connexió s'utilitza el mètode cursor()
 connection = conn.cursor()
 
+sql = '''CREATE TABLE USERS(
+            user_id SERIAL PRIMARY KEY,
+            user_name VARCHAR(255) NOT NULL,
+            user_surname VARCHAR(255) NOT NULL,
+            user_age BIGINT NOT NULL,
+            user_email VARCHAR(255) NOT NULL
+            )'''
+
+print(sql)
+
+connection.execute(sql)
 print(connection)
