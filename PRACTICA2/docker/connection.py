bb90ab5975690d49397bff48ce88dd49fab70b88
Oriol Martinez <2023_oriol.martinez@iticbcn.cat>
2024-02-03 11:10:49 +0100

    Modificant fitxers de connexió i eliminant clase main d'aquesta branca. Falta comentaris

--- PRACTICA2/docker/connection.py
+++ PRACTICA2/docker/connection.py
@@ -1,27 +1,12 @@
 import psycopg2
 
 # Per saber quina és la database, el user i el password mirar el .yml
-conn = psycopg2.connect(
-    database="",
-    user="",
-    password="",
-    host="",
+def establir_connexio():
+    mydb = psycopg2.connect(
+    database="postgres",
+    user="user_postgres",
+    password="pass_postgres",
     host="localhost",
-    port="5432"
-)
+    port="5432")
 
-# Per fer la connexió s'utilitza el mètode cursor()
-connection = conn.cursor()
-
-sql = '''CREATE TABLE USERS(
-            user_id SERIAL PRIMARY KEY,
-            user_name VARCHAR(255) NOT NULL,
-            user_surname VARCHAR(255) NOT NULL,
-            user_age BIGINT NOT NULL,
-            user_email VARCHAR(255) NOT NULL
-            )'''
-
-print(sql)
-
-connection.execute(sql)
-print(connection)
+    return mydb.cursor()
