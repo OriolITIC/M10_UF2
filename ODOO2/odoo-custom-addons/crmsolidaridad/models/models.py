1aa2b8f6561a9b3b451c8ab4f9dc9abc8952c980
oriol <2023_oriol.martinez@iticbcn.cat>
2024-05-09 17:19:29 +0200

    Probant de instalar mòdul marketing per email

--- ODOO2/odoo-custom-addons/crmsolidaridad/models/models.py
+++ ODOO2/odoo-custom-addons/crmsolidaridad/models/models.py
@@ -1,18 +1,31 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class crmsolidaridad(models.Model):
-#     _name = 'crmsolidaridad.crmsolidaridad'
-#     _description = 'crmsolidaridad.crmsolidaridad'
+class crmsolidaridad(models.Model):
+    _name = 'crm.lead'
+    _description = 'CRM Lead'
+    TIPO_CLIENTE =[
+        ('empresa','Empresa'),
+        ('individual','Individual'),
+        ('otro','Otro')
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+
+    ]
+
+
+    name = fields.Text(string='Name',required=True)
+    email = fields.Char(string='Email')
+    phone = fields.Char(string='Phone')
+    tipo_cliente = fields.Selection(
+        selection=TIPO_CLIENTE,
+        string='Tipo de cliente'
+    )
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
