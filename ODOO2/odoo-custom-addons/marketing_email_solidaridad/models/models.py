78b77380cce409176fad025c8cb11aa4f7a6a53a
oriol <2023_oriol.martinez@iticbcn.cat>
2024-05-11 17:10:10 +0200

    Haciendo modulo Marketing digital

--- ODOO2/odoo-custom-addons/marketing_email_solidaridad/models/models.py
+++ ODOO2/odoo-custom-addons/marketing_email_solidaridad/models/models.py
@@ -2,7 +2,7 @@
 
 from odoo import models, fields, api
 
-
+"""
 class marketing_email_solidaridad(models.Model):
     _name = 'marketing_email_solidaridad.marketing_email_solidaridad'
     _description = 'marketing_email_solidaridad.marketing_email_solidaridad'
@@ -15,4 +15,28 @@ class marketing_email_solidaridad(models.Model):
     @api.depends('value')
     def _value_pc(self):
         for record in self:
-            record.value2 = float(record.value) / 100
+            record.value2 = float(record.value) / 100"""
+
+
+class Empresa(models.Model):
+    _name = 'marketing_digital.empresa'
+    _description = 'Información de la Empresa'
+
+
+    name = fields.Char(string='Nombre')
+    email = fields.Char(string='Email')
+    telefono = fields.Char(string='Teléfono')
+
+class CorreoEnviado(models.Model):
+    _name = 'marketing_digital.correo_enviado'
+    _description = 'Correos Enviados'
+
+    
+    email = fields.Char(string='Email')
+    asunto = fields.Char(string='Asunto')
+    mensaje = fields.Text(string='Mensaje')
+    photo = fields.Binary(string='Foto')
+    fecha = fields.Date(string='Fecha')
+
+
+    empresa_id = fields.Many2one('marketing_digital.empresa', string='Empresa')
