0de0be5a82fd1a277091e53e06ac88539bb27226
oriol <2023_oriol.martinez@iticbcn.cat>
2024-05-14 16:01:14 +0200

    cambios modulo crm  Ref #44

--- ODOO2/odoo-custom-addons/crmsolidaridad/models/models.py
+++ ODOO2/odoo-custom-addons/crmsolidaridad/models/models.py
@@ -1,18 +1,59 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class crmsolidaridad(models.Model):
-#     _name = 'crmsolidaridad.crmsolidaridad'
-#     _description = 'crmsolidaridad.crmsolidaridad'
+class crmsolidaridad(models.Model):
+    _name = 'crmsolidaridad.crmsolidaridad'
+    _description = 'crmsolidaridad.crmsolidaridad'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
+
+
+class CustomCRMClient(models.Model):
+    _name = 'custom.crm.client'
+    _description = 'Custom CRM Client'
+    #_description = 'Pipeline'
+
+    name = fields.Char(string='Name', required=True)
+    email = fields.Char(string='Email')
+    phone = fields.Char(string='Phone')
+    client_type = fields.Selection([('individual', 'Individual'), ('company', 'Company')], string='Client Type', default='individual')
+
+    @api.model
+    def create_crm_client(self, name, email=None, phone=None, client_type='individual'):
+        vals = {
+            'name': name,
+            'email': email,
+            'phone': phone,
+            'client_type': client_type,
+        }
+        return self.create(vals)
+    
+"""" 
+class CustomCRMPipeline(models.Model):
+    _name = 'custom.crm.pipeline'
+    _description = 'Custom CRM Client'
+    #_description = 'Pipeline'
+
+    name = fields.Char(string='Nuevo')
+
+  
+    @api.model
+    def create_crm_pipeline_nuevo(self, name, email=None, phone=None, client_type='individual'):
+        vals = {
+            'name': name,
+            'email': email,
+            'phone': phone,
+            'client_type': client_type,
+        }
+        return self.create(vals)"""   
+
