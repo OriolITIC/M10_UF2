fda1a8d4c07a64a8e197fbbac4867ca3661b4ee7
AleksandraJ22 <2023_aleksandra.jovanovic@iticbcn.cat>
2024-05-14 17:14:59 +0200

    Mas cambios modilo CRM creacion oportunidad cliente Ref #44

--- ODOO2/odoo-custom-addons/crmsolidaridad/models/models.py
+++ ODOO2/odoo-custom-addons/crmsolidaridad/models/models.py
@@ -38,22 +38,18 @@ class CustomCRMClient(models.Model):
         }
         return self.create(vals)
     
-"""" 
+
 class CustomCRMPipeline(models.Model):
     _name = 'custom.crm.pipeline'
-    _description = 'Custom CRM Client'
-    #_description = 'Pipeline'
+    _description = 'Custom CRM Pipeline'
 
-    name = fields.Char(string='Nuevo')
+    name = fields.Char(string='Title', required=True)
+    cliente_id = fields.Many2one('custom.crm.client', string='Cliente', required=True)
 
-  
     @api.model
-    def create_crm_pipeline_nuevo(self, name, email=None, phone=None, client_type='individual'):
+    def create_crm_pipeline_nuevo(self, name, cliente_id):
         vals = {
             'name': name,
-            'email': email,
-            'phone': phone,
-            'client_type': client_type,
+            'cliente_id': cliente_id,
         }
-        return self.create(vals)"""   
-
+        return self.create(vals)
